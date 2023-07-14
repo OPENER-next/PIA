93a39b41f4fae01ca6f407f30b0b3a568cdce8b5
Robbendebiene <robbendebiene@mailbox.org>
2023-07-14 16:13:50 +0200

    Add helper methods to position class

--- lib/shared/models/position.dart
+++ lib/shared/models/position.dart
@@ -9,4 +9,16 @@ class Position extends LatLng {
   const Position(super.latitude, super.longitude, {
     this.level = Level.zero,
   });
+
+  @override
+  Map<String, double> toJson() => {
+    'lat': latitude,
+    'lng': longitude,
+  };
+
+  factory Position.fromGeoJsonCoordinates(List<double> json) =>
+    Position(json[1], json[0]);
+
+  @override
+  String toString() => 'Position(lat: $latitude, lon: $longitude, level: $level)';
 }
