4bab15c04b698d3a1790ec80fda7f6ab1f7bebc6
Robbendebiene <robbendebiene@mailbox.org>
2024-06-07 10:56:26 +0200

    Improve listenable to observable adapter Allows the usage for animation state changes

--- lib/shared/utils/listenable_observable_adapter.dart
+++ lib/shared/utils/listenable_observable_adapter.dart
@@ -6,17 +6,30 @@ import 'package:flutter/foundation.dart';
 class ListenableObservableAdapter<T extends Listenable> {
   late final Atom _atom;
   final T _listenable;
+  final void Function(T listenable, VoidCallback reportChanged) onObserved;
+  final void Function(T listenable, VoidCallback reportChanged) onUnobserved;
 
-  ListenableObservableAdapter(this._listenable) {
+  ListenableObservableAdapter(this._listenable, {
+    this.onObserved = defaultOnObservedCallback,
+    this.onUnobserved = defaultOnUnobservedCallback,
+  }) {
     _atom = Atom(
       onObserved: _track,
       onUnobserved: _untrack,
     );
   }
 
-  void _track() =>_listenable.addListener(_atom.reportChanged);
+  static void defaultOnObservedCallback(Listenable listenable, VoidCallback reportChanged) {
+    listenable.addListener(reportChanged);
+  }
 
-  void _untrack() => _listenable.removeListener(_atom.reportChanged);
+  static void defaultOnUnobservedCallback(Listenable listenable, VoidCallback reportChanged) {
+    listenable.removeListener(reportChanged);
+  }
+
+  void _track() => onObserved(_listenable, _atom.reportChanged);
+
+  void _untrack() => onUnobserved(_listenable, _atom.reportChanged);
 
   T get listenable {
     _atom.reportRead();
